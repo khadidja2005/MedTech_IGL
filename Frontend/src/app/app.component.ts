1a54492424591fc40f03afa3945d9d50e7890b5a
samah-c <ms_mahmoudi@esi.dz>
2024-12-14 14:29:58 +0100

    advantages added

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './LandingPage/header/header.component';
 import { MainContentComponent } from './LandingPage/main-content/main-content.component';
+import { AdvantagesComponent } from './LandingPage/advantages/advantages.component';
 
 @Component({
   selector: 'app-root',
   standalone: true, 
-  imports: [RouterOutlet, HeaderComponent, MainContentComponent],  
+  imports: [RouterOutlet, HeaderComponent, MainContentComponent, AdvantagesComponent],  
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
