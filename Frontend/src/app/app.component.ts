8029fc3f78c3c129f83953dd52fced2a9ee1b93e
samah-c <ms_mahmoudi@esi.dz>
2024-12-14 17:01:04 +0100

    sam's third

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './LandingPage/header/header.component';
 import { MainContentComponent } from './LandingPage/main-content/main-content.component';
 import { AdvantagesComponent } from './LandingPage/advantages/advantages.component';
+import {MedtechDefComponent} from './LandingPage/medtech-def/medtech-def.component'
 
 @Component({
   selector: 'app-root',
   standalone: true, 
-  imports: [RouterOutlet, HeaderComponent, MainContentComponent, AdvantagesComponent],  
+  imports: [RouterOutlet, HeaderComponent, MainContentComponent, AdvantagesComponent, MedtechDefComponent],  
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
