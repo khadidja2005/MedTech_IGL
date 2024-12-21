76691eeb650ce2e772707fd5d66f44692c5a146f
unknown <ms_mahmoudi@esi.dz>
2024-12-21 16:50:36 +0100

    sam & mia

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import {LandingPageComponent} from './LandingPage/landing-page/landing-page.component';
-import {HeaderPDIComponent} from './PDI/header-pdi/header-pdi.component';
+import {HeaderPDIComponent} from './components/header-pdi/header-pdi.component';
+import { SidebarComponent } from "./components/sidebar/sidebar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true, 
-  imports: [RouterOutlet,LandingPageComponent,HeaderPDIComponent],  
+  imports: [RouterOutlet, LandingPageComponent, HeaderPDIComponent, SidebarComponent],  
 
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
