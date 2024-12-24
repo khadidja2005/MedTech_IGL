d574fb2d63d67971fdea99a8c400feaf84cc630e
unknown <ms_mahmoudi@esi.dz>
2024-12-24 09:25:07 +0100

    finalll pdii

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -4,12 +4,13 @@ import {LandingPageComponent} from './LandingPage/landing-page/landing-page.comp
 import {HeaderPDIComponent} from './components/header-pdi/header-pdi.component';
 import { SidebarComponent } from "./components/sidebar/sidebar.component";
 import {DpiInfoComponent} from "./PDI/dpi-info/dpi-info.component";
+import { DpiComponent } from "./PDI/dpi/dpi.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true, 
-  imports: [RouterOutlet, LandingPageComponent, HeaderPDIComponent, SidebarComponent, DpiInfoComponent],  
+  imports: [RouterOutlet, LandingPageComponent, HeaderPDIComponent, SidebarComponent, DpiInfoComponent, DpiComponent],  
 
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
