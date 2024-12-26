d4dde01fabb00a0ba483640a9c4cc15bb5570b53
unknown <ms_mahmoudi@esi.dz>
2024-12-26 03:01:51 +0100

    etab

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -6,12 +6,13 @@ import { SidebarComponent } from "./components/sidebar/sidebar.component";
 import {DpiInfoComponent} from "./PDI/dpi-info/dpi-info.component";
 import { DpiComponent } from "./PDI/dpi/dpi.component";
 import { ConsultationInfoComponent } from "./Consultation/consultation-info/consultation-info.component";
+import { EstablishmentsComponent } from "./dashboard-etablissement/establishments/establishments.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true, 
-  imports: [RouterOutlet, LandingPageComponent, HeaderPDIComponent, SidebarComponent, DpiInfoComponent, DpiComponent, ConsultationInfoComponent],  
+  imports: [RouterOutlet, LandingPageComponent, HeaderPDIComponent, SidebarComponent, DpiInfoComponent, DpiComponent, ConsultationInfoComponent, EstablishmentsComponent],  
 
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
