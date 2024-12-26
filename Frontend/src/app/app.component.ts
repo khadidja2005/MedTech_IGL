915b8fa00158e141bc45ffcf4c63f02b3cafaf3c
Miashere <mm_rouabhi@esi.dz>
2024-12-26 17:58:49 +0100

    Archive Radio done

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LaborantinComponent } from './Laborantin/laborantin/laborantin.component';
-import { ArchiveComponent } from './LaborantinArchive/archive/archive.component';
-import { RadiologueComponent } from './Radiologue/radiologue/radiologue.component';
+import { ArchiveComponent } from './RadiologueArchive/archive/archive.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RadiologueComponent],
+  imports: [RouterOutlet, ArchiveComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
