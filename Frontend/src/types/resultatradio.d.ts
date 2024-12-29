d00e55b780f888236bb663d17376cf8e3a7d1e0a
CherazBerkat <142236701+CherazBerkat@users.noreply.github.com>
2024-12-29 15:01:49 +0100

    Update resultatradio.d.ts

--- Frontend/src/types/resultatradio.d.ts
+++ Frontend/src/types/resultatradio.d.ts
@@ -1,8 +1,4 @@
 export interface ResultatRadio {
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 5f0d9f979522c5b5947c25fbbca8ebf379e3497a
     id: string;
     description: string;
     piece_jointe: string;
@@ -12,16 +8,3 @@ export interface ResultatRadio {
     radiologue: string; // Foreign key to PersonnelMedical
     
 }
-<<<<<<< HEAD
-=======
-  id: number;
-  description: string;
-  piece_jointe: string;
-  date: string; // ISO date string
-  compte_rendu: string | null;
-  radiologue_compte_rendu: string | null; // Foreign key to PersonnelMedical
-  radiologue: string; // Foreign key to PersonnelMedical
-}
->>>>>>> 171d1ec9eab303e010f3075fd91ce940b712df3a
-=======
->>>>>>> 5f0d9f979522c5b5947c25fbbca8ebf379e3497a
