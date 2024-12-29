5787c63c5c3ae11de601817b8a6a89a4e16fac90
CherazBerkat <142236701+CherazBerkat@users.noreply.github.com>
2024-12-29 14:59:22 +0100

    Update bilanbio.d.ts

--- Frontend/src/types/bilanbio.d.ts
+++ Frontend/src/types/bilanbio.d.ts
@@ -1,8 +1,4 @@
 export interface BilanBio {
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 5f0d9f979522c5b5947c25fbbca8ebf379e3497a
     id: string;
     date_debut: string; // ISO date string
     date_fin: string; // ISO date string
@@ -14,16 +10,4 @@ export interface BilanBio {
     etablissement:string;
     patient:string;
 }
-<<<<<<< HEAD
-=======
-  id: number;
-  date_debut: string; // ISO date string
-  date_fin: string | null; // ISO date string
-  parametres: string;
-  est_complet: boolean;
-  est_resultat: boolean;
-  Consultation: string | null; // Foreign key to Consultation, nullable
-}
->>>>>>> 171d1ec9eab303e010f3075fd91ce940b712df3a
-=======
->>>>>>> 5f0d9f979522c5b5947c25fbbca8ebf379e3497a
+
